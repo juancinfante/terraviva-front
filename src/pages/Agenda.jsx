e24a1694a6dc9686f9ab96f258b3be8c0cc0e4b0
juancinfante <juaninfantejj@gmail.com>
2024-07-05 19:21:49 -0300

    agenda fix

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -168,7 +168,7 @@ const Agenda = () => {
                                             <a href="/agenda/12/1">Todas</a>
                                         </li>
                                         <li>
-                                            <a href="/agenda/Buenos Aires/12/1">Buenos Aires</a>
+                                            <a href="/agenda/Buenos aires/12/1">Buenos Aires</a>
                                         </li>
                                         <li>
                                             <a href="/agenda/Capital Federal/12/1">Capital Federal</a>
