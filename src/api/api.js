3e122f3efa84c925b7c2bed111d3b564e763a5ba
juancinfante <juaninfantejj@gmail.com>
2024-08-12 11:05:22 -0300

    volviendo a vercel xD

--- src/api/api.js
+++ src/api/api.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 const api = axios.create({
     // baseURL: 'https://terraviva-back.vercel.app/',
     // baseURL: 'http://localhost:4001/',
-    baseURL: 'https://terraviva-back-sybt-juancinfante-juancinfantes-projects.vercel.app/',
+    // baseURL: 'https://terraviva-back-sybt-juancinfante-juancinfantes-projects.vercel.app/',
     // baseURL: 'https://terraviva-api.onrender.com/',
+    baseURL: 'https://terraviva-api-new.vercel.app/',
+    
 })
 
 
