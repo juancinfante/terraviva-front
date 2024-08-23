34b877054aea4e45fbc7b28e3bb95329004dc3ee
Juan Cruz Infante <48840855+juancinfante@users.noreply.github.com>
2024-08-23 11:37:40 -0300

    Update BannerSantiago.jsx

--- src/components/BannerSantiago.jsx
+++ src/components/BannerSantiago.jsx
@@ -11,24 +11,10 @@ const BannerSantiago = () => {
         //     </div>
         // </div>
         <>
-            <div className='container d-none d-lg-block'>
-                <div style={{ width: "100%" }}>
-                    <img src={bannerDemiViolinero} className='w-100 mt-5 banner-sgo' />
-                </div>
-            </div>
             <div className='container d-block d-lg-none' >
-               
                     <div style={{ width: "100%" }}>
                         <img src={banner} className='w-100 mt-4 banner-sgo' style={{ height: 80 }} />
                     </div>
-                
-            </div>
-            <div className='container d-block d-lg-none'>
-                <a href="https://terraviva.com.ar/evento/66a96625139f304971389307">
-                    <div style={{ width: "100%" }}>
-                        <img src={bannerDemi} className='w-100 mt-4 banner-sgo' style={{ height: 80 }} />
-                    </div>
-                </a>
             </div>
         </>
     )
