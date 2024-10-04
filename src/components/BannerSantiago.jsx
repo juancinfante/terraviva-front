3f8adb766f9f2b5bd138dcd848ffe0066b0d39de
juancinfante <juaninfantejj@gmail.com>
2024-10-04 10:03:35 -0300

    banner raly añadidos

--- src/components/BannerSantiago.jsx
+++ src/components/BannerSantiago.jsx
@@ -1,25 +1,28 @@
 /* eslint-disable no-unused-vars */
 import banner from '../assets/nuevo-banner.jpg'
+import banner_terra_mobile from '../assets/banner_terra_mobile.jpg'
 import bannerDemi from '../assets/demi.jpg'
 import bannerViolinero from '../assets/violinero.jpg'
 import bannerDemiViolinero from '../assets/demi-violinero.jpg'
+import banner_raly from '../assets/ralyquimsa.png'
+import banner_raly_mobile from '../assets/ralyquimsa_mobile.png'
+
 const BannerSantiago = () => {
     return (
-        // <div className='container'>
-        //     <div style={{ width: "100%" }}>
-        //         <img src={banner} className='w-100 mt-5 banner-sgo'/>
-        //     </div>
-        // </div>
         <>
             <div className='container d-none d-md-block' >
-                    <div style={{ width: "100%" }}>
-                        <img src={banner} className='w-100 mt-4 banner-sgo' style={{ height: 110 }} />
-                    </div>
+                <div style={{ width: "100%" }}>
+                    <a href="https://www.entradaweb.com.ar/evento/e52085ec/step/1" target='_blank' rel='noreferrer'>
+                        <img src={banner_raly} className='w-100 mt-4 banner-sgo' style={{ height: 120 }} />
+                    </a>
+                </div>
             </div>
             <div className='container d-block d-md-none' >
-                    <div style={{ width: "100%" }}>
-                        <img src={banner} className='w-100 mt-4 banner-sgo' style={{ height: 70 }} />
-                    </div>
+                <div style={{ width: "100%" }}>
+                    <a href="https://www.entradaweb.com.ar/evento/e52085ec/step/1" target='_blank' rel='noreferrer'>
+                        <img src={banner_raly_mobile} className='w-100 mt-4 banner-sgo' style={{ height: 130 }} />
+                    </a>
+                </div>
             </div>
         </>
     )
