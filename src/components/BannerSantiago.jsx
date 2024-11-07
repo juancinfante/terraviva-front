66a09c8ef73f62fe8efe570022a666b72c9163db
juancinfante <juaninfantejj@gmail.com>
2024-11-07 19:48:37 -0300

    banner actualizado

--- src/components/BannerSantiago.jsx
+++ src/components/BannerSantiago.jsx
@@ -6,6 +6,8 @@ import bannerViolinero from '../assets/violinero.jpg'
 import bannerDemiViolinero from '../assets/demi-violinero.jpg'
 import banner_raly from '../assets/ralyquimsa.png'
 import banner_raly_mobile from '../assets/ralyquimsa_mobile.png'
+import ol_ban_cen from '../assets/OL-ban-cen.jpg'
+import ol_ban_mob from '../assets/OL-ban-mob.jpg'
 
 const BannerSantiago = () => {
     return (
@@ -13,14 +15,14 @@ const BannerSantiago = () => {
             <div className='container d-none d-md-block' >
                 <div style={{ width: "100%" }}>
                     <a href="" target='_blank' rel='noreferrer'>
-                        <img src={banner} className='w-100 mt-4 banner-sgo' style={{ height: 120 }} />
+                        <img src={ol_ban_cen} className='w-100 mt-4 banner-sgo' style={{ height: 120 }} />
                     </a>
                 </div>
             </div>
             <div className='container d-block d-md-none' >
                 <div style={{ width: "100%" }}>
                     <a href="" target='_blank' rel='noreferrer'>
-                        <img src={banner_terra_mobile} className='w-100 mt-4 banner-sgo' style={{ height: 130 }} />
+                        <img src={ol_ban_mob} className='w-100 mt-4 banner-sgo' style={{ height: 130 }} />
                     </a>
                 </div>
             </div>
