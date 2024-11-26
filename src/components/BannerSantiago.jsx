7259caaca9be298f82004dad8bc9d3643200060a
juancinfante <juaninfantejj@gmail.com>
2024-11-26 16:21:02 -0300

    se creo el componente redes

--- src/components/BannerSantiago.jsx
+++ src/components/BannerSantiago.jsx
@@ -9,6 +9,7 @@ import banner_raly_mobile from '../assets/ralyquimsa_mobile.png'
 import ol_ban_cen from '../assets/OL-ban-cen.jpg'
 import ol_ban_mob from '../assets/OL-ban-mob.jpg'
 import ol_nuevo from '../assets/BANNER-CENTRAL-ol_ol.jpg'
+import ol_nuevo_mobile from '../assets/BANNER-CENTRAL-ol_ol_mobile.jpg'
 
 const BannerSantiago = () => {
     return (
@@ -23,7 +24,7 @@ const BannerSantiago = () => {
             <div className='container d-block d-md-none' >
                 <div style={{ width: "100%" }}>
                     <a href="https://www.entradaweb.com.ar/evento/7ebd62f2/step/1" target='_blank' rel='noreferrer'>
-                        <img src={ol_ban_mob} className='w-100 mt-4 banner-sgo' style={{ height: 130 }} />
+                        <img src={ol_nuevo_mobile} className='w-100 mt-4 banner-sgo' style={{ height: 130 }} />
                     </a>
                 </div>
             </div>
