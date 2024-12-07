8fb33a8ff23ab0eb1ee90df00e78ac9d8fdc87b3
juancinfante <juaninfantejj@gmail.com>
2024-12-07 19:56:14 -0300

    banner

--- src/components/BannerSantiago.jsx
+++ src/components/BannerSantiago.jsx
@@ -10,21 +10,23 @@ import ol_ban_cen from '../assets/OL-ban-cen.jpg'
 import ol_ban_mob from '../assets/OL-ban-mob.jpg'
 import ol_nuevo from '../assets/BANNER-CENTRAL-ol_ol.jpg'
 import ol_nuevo_mobile from '../assets/BANNER-CENTRAL-ol_ol_mobile.jpg'
+import BanC_CHACA25_movil from '../assets/BanC_CHACA25_movil.jpg'
+import BanC_CHACARERA25 from '../assets/BanC_CHACARERA25.jpg'
 
 const BannerSantiago = () => {
     return (
         <>
             <div className='container d-none d-md-block' >
                 <div style={{ width: "100%" }}>
-                    <a target='_blank' rel='noreferrer'>
-                        <img src={banner} className='w-100 mt-4 banner-sgo' style={{ height: 120 }} />
+                    <a href="https://www.entradaweb.com.ar/eventos?group=272" target='_blank' rel='noreferrer'>
+                        <img src={BanC_CHACARERA25} className='w-100 mt-4 banner-sgo' style={{ height: 120 }} />
                     </a>
                 </div>
             </div>
             <div className='container d-block d-md-none' >
                 <div style={{ width: "100%" }}>
-                    <a target='_blank' rel='noreferrer'>
-                        <img src={banner_terra_mobile} className='w-100 mt-4 banner-sgo' style={{ height: 130 }} />
+                    <a href="https://www.entradaweb.com.ar/eventos?group=272" target='_blank' rel='noreferrer'>
+                        <img src={BanC_CHACA25_movil} className='w-100 mt-4 banner-sgo' style={{ height: 130 }} />
                     </a>
                 </div>
             </div>
