050cada80db88da7bbccb6c5a64777a2bcfa7951
juancinfante <juaninfantejj@gmail.com>
2025-01-22 15:50:26 -0300

    tipically

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'https://terraviva-back.vercel.app/',
-    baseURL: 'http://localhost:4001/',
+    // baseURL: 'http://localhost:4001/',
     // baseURL: 'https://terraviva-back-sybt-juancinfante-juancinfantes-projects.vercel.app/',
     // baseURL: 'https://terraviva-api.onrender.com/',
-    // baseURL: 'https://terraviva-api-new.vercel.app/',
+    baseURL: 'https://terraviva-api-new.vercel.app/',
     
 })
 
